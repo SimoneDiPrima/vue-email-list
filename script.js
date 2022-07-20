da0d215c933bac6d88ed9d8ea66e0ba487ea0645
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-20 15:26:45 +0200

    sezione bonus

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ console.log(`vue ok`,Vue);
 const root = new Vue({
     el:`#root`,
     data:{
+        allEmail:  9,
         emails:[]
     },
-    created(){
-        for(let i=1;i<= 10; i++ ){
-            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-            .then((answer)=>{
-                const email = answer.data.response
-                this.emails.push(email)
-            })
-
+    methods:{
+        getrandomEmail(){
+            for(let i=1;i<= 10; i++ ){
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                .then((answer)=>{
+                    const email = answer.data.response
+                    this.emails.push(email)
+                })
+    
+            }
         }
+    },
+    created(){
+       return this.getrandomEmail(`email`)
     }
     
 
