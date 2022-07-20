20ee16a77f0fcf54fe74ca07d83a783ab375814b
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-20 14:46:13 +0200

    esercizio senza bonus

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log(`vue ok`,Vue);
+
+const root = new Vue({
+    el:`#root`,
+    data:{
+        emails:[]
+    },
+    created(){
+        for(let i=1;i<= 10; i++ ){
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then((answer)=>{
+                const email = answer.data.response
+                this.emails.push(email)
+            })
+
+        }
+    }
+    
+
+
+
+
+
+})
